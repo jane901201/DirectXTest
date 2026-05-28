16f8e469a26b9bfbfd2c077afc51fd3fd0f52ddd
SMARTES1004-2\User <jane36742715@gmail.com>
2026-05-28 17:43:09 +0800

    存檔

---
+++ MyEngine/core/CelShader.cpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <d3d12.h>
+#include <DirectXMath.h>
+#include <wrl/client.h>
+#include <cstdint>
+#include <cstring>
+
+using Microsoft::WRL::ComPtr;
+
+static constexpr uint32_t AlignConstantBufferSize(uint32_t size) {
+    return (size + 255) & ~255;
+}
+
